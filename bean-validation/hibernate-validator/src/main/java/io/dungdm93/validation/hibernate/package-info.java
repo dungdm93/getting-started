f49d18b710d54b3f038f0093fa510f13cea41749
dungdm93 <dungdm93@live.com>
2016-01-28 16:45:34 +0700

    update doc

---
+++ bean-validation/hibernate-validator/src/main/java/io/dungdm93/validation/hibernate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <ul>
+ * <li><a href="http://docs.jboss.org/hibernate/stable/validator/reference/en-US/html_single/">Documentation</a></li>
+ * <li><a href="https://github.com/hibernate/hibernate-validator/tree/master/documentation/src/test">Sample Repo</a></li>
+ * </ul>
+ */
+package io.dungdm93.validation.hibernate;
