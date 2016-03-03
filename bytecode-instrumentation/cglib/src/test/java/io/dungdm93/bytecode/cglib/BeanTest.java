91956fe4de1cc49c36f1bb84959c16a790fcbe7d
dungdm93 <dungdm93@live.com>
2016-03-04 01:01:11 +0700

    BeanGenerator

--- bytecode-instrumentation/cglib/src/test/java/io/dungdm93/bytecode/cglib/BeanTest.java
+++ bytecode-instrumentation/cglib/src/test/java/io/dungdm93/bytecode/cglib/BeanTest.java
@@ -1,7 +1,10 @@
 package io.dungdm93.bytecode.cglib;
 
+import net.sf.cglib.beans.BeanGenerator;
 import net.sf.cglib.beans.ImmutableBean;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,5 +22,18 @@ public class BeanTest {
         assertEquals("Hello world, again!", immutableBean.getValue());
 
         immutableBean.setValue("Hello cglib!"); // Causes exception.
+    }
+
+    @Test
+    public void beanGenerator() throws Exception {
+        BeanGenerator beanGenerator = new BeanGenerator();
+        beanGenerator.addProperty("value", String.class);
+        Object bean = beanGenerator.create();
+        System.out.println(bean.toString());
+
+        Method setter = bean.getClass().getMethod("setValue", String.class);
+        setter.invoke(bean, "Hello cglib!");
+        Method getter = bean.getClass().getMethod("getValue");
+        assertEquals("Hello cglib!", getter.invoke(bean));
     }
 }
