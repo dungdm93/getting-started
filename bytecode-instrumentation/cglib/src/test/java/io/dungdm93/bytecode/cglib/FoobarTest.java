acdc6313b4c5a94dd9ce464408d6dead7554e949
dungdm93 <dungdm93@live.com>
2016-03-03 23:11:14 +0700

    InvocationHandler

--- bytecode-instrumentation/cglib/src/test/java/io/dungdm93/bytecode/cglib/FoobarTest.java
+++ bytecode-instrumentation/cglib/src/test/java/io/dungdm93/bytecode/cglib/FoobarTest.java
@@ -27,4 +27,24 @@ public class FoobarTest {
         System.out.println(proxy.getClass());
         assertEquals("Hello cglib!", proxy.doStaff());
     }
+
+    @Test
+    public void invocationHandler() throws Exception {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Foobar.class);
+        enhancer.setCallback(new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args)
+                    throws Throwable {
+                if (!method.getDeclaringClass().equals(Object.class) && method.getReturnType().equals(String.class)) {
+                    return "Hello cglib!";
+                } else {
+                    return "Do not know what to do.";
+                }
+            }
+        });
+        Foobar proxy = (Foobar) enhancer.create();
+        assertEquals("Hello cglib!", proxy.doStaff());
+        assertNotEquals("Hello cglib!", proxy.toString());
+    }
 }
