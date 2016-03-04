3e91e587999630d3466c548a4dea726d6242326f
dungdm93 <dungdm93@live.com>
2016-03-04 12:56:17 +0700

    MulticastDelegate

---
+++ bytecode-instrumentation/cglib/src/main/java/io/dungdm93/bytecode/cglib/delegate/MulticastDeputy.java
@@ -0,0 +1,5 @@
+package io.dungdm93.bytecode.cglib.delegate;
+
+public interface MulticastDeputy {
+    void setValue(String value);
+}
--- bytecode-instrumentation/cglib/src/test/java/io/dungdm93/bytecode/cglib/DelegateTest.java
+++ bytecode-instrumentation/cglib/src/test/java/io/dungdm93/bytecode/cglib/DelegateTest.java
@@ -1,7 +1,9 @@
 package io.dungdm93.bytecode.cglib;
 
 import io.dungdm93.bytecode.cglib.delegate.MethodDeputy;
+import io.dungdm93.bytecode.cglib.delegate.MulticastDeputy;
 import net.sf.cglib.reflect.MethodDelegate;
+import net.sf.cglib.reflect.MulticastDelegate;
 import org.junit.Test;
 
 import java.util.function.Supplier;
@@ -20,5 +22,20 @@ public class DelegateTest {
         @SuppressWarnings("unchecked")
         Supplier<String> supplier = (Supplier) MethodDelegate.create(bean, "getValue", Supplier.class);
         assertEquals("Hello cglib!", supplier.get());
+    }
+
+    @Test
+    public void multicastDelegate() throws Exception {
+        MulticastDelegate delegate = MulticastDelegate.create(MulticastDeputy.class);
+        Bean first = new Bean();
+        Bean second = new Bean();
+        delegate = delegate.add(first);
+        delegate = delegate.add(second);
+
+        MulticastDeputy provider = (MulticastDeputy) delegate;
+        provider.setValue("Hello world!");
+
+        assertEquals("Hello world!", first.getValue());
+        assertEquals("Hello world!", second.getValue());
     }
 }
