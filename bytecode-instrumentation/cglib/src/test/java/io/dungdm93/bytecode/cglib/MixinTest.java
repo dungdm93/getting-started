4ee8f538213f2fc02844e1ac6332ababde60edad
dungdm93 <dungdm93@live.com>
2016-03-04 12:46:07 +0700

    InterfaceMaker

--- bytecode-instrumentation/cglib/src/test/java/io/dungdm93/bytecode/cglib/MixinTest.java
+++ bytecode-instrumentation/cglib/src/test/java/io/dungdm93/bytecode/cglib/MixinTest.java
@@ -2,9 +2,12 @@ package io.dungdm93.bytecode.cglib;
 
 import io.dungdm93.bytecode.cglib.mixin.*;
 import net.sf.cglib.core.KeyFactory;
+import net.sf.cglib.core.Signature;
+import net.sf.cglib.proxy.InterfaceMaker;
 import net.sf.cglib.proxy.Mixin;
 import net.sf.cglib.util.StringSwitcher;
 import org.junit.Test;
+import org.objectweb.asm.Type;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -40,5 +43,17 @@ public class MixinTest {
         assertEquals(1, stringSwitcher.intValue("one"));
         assertEquals(2, stringSwitcher.intValue("two"));
         assertEquals(-1, stringSwitcher.intValue("three"));
+    }
+
+    @Test
+    public void interfaceMaker() throws Exception {
+        InterfaceMaker interfaceMaker = new InterfaceMaker();
+        Signature signature = new Signature("foo", Type.DOUBLE_TYPE, new Type[]{Type.INT_TYPE});
+        interfaceMaker.add(signature, new Type[0]);
+        Class iface = interfaceMaker.create();
+
+        assertEquals(1, iface.getMethods().length);
+        assertEquals("foo", iface.getMethods()[0].getName());
+        assertEquals(double.class, iface.getMethods()[0].getReturnType());
     }
 }
