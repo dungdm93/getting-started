c712ead597aef634fef54f700136d4a0c49239cc
dungdm93 <dungdm93@live.com>
2016-03-04 13:00:36 +0700

    ConstructorDelegate

---
+++ bytecode-instrumentation/cglib/src/main/java/io/dungdm93/bytecode/cglib/delegate/ConstructorDeputy.java
@@ -0,0 +1,6 @@
+package io.dungdm93.bytecode.cglib.delegate;
+
+public interface ConstructorDeputy {
+    // This method signature is reserved
+    Object newInstance();
+}
--- bytecode-instrumentation/cglib/src/test/java/io/dungdm93/bytecode/cglib/DelegateTest.java
+++ bytecode-instrumentation/cglib/src/test/java/io/dungdm93/bytecode/cglib/DelegateTest.java
@@ -2,6 +2,8 @@ package io.dungdm93.bytecode.cglib;
 
 import io.dungdm93.bytecode.cglib.delegate.MethodDeputy;
 import io.dungdm93.bytecode.cglib.delegate.MulticastDeputy;
+import io.dungdm93.bytecode.cglib.delegate.ConstructorDeputy;
+import net.sf.cglib.reflect.ConstructorDelegate;
 import net.sf.cglib.reflect.MethodDelegate;
 import net.sf.cglib.reflect.MulticastDelegate;
 import org.junit.Test;
@@ -9,6 +11,7 @@ import org.junit.Test;
 import java.util.function.Supplier;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class DelegateTest {
     @Test
@@ -37,5 +40,14 @@ public class DelegateTest {
 
         assertEquals("Hello world!", first.getValue());
         assertEquals("Hello world!", second.getValue());
+    }
+
+    @Test
+    public void constructorDelegate() throws Exception {
+        ConstructorDeputy delegate = (ConstructorDeputy) ConstructorDelegate.create(Bean.class, ConstructorDeputy.class);
+        Bean bean = (Bean) delegate.newInstance();
+
+        assertTrue(Bean.class.isAssignableFrom(bean.getClass()));
+        System.out.println(bean.getClass());
     }
 }
